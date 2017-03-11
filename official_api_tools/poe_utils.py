134c9361aab2821d4bb6f5869eaa07f3d902f0d4
Steve Brown <steve@stevejbrown.com>
2017-03-11 12:13:29 -0800

    Used double quotes for docstrings and removed pass statements

--- official_api_tools/poe_utils.py
+++ official_api_tools/poe_utils.py
@@ -5,15 +5,15 @@ import pathofexile.pathofexile.ladder as poe_ladder
 
 
 def write_league_to_table(league, cursor, table):
-    ''' Writes a single league into an sqlite table
+    """ Writes a single league into an sqlite table
     
-        :param league: Dictionary. Should contain 'startAt', 'description',
-            'registerAt', 'url', 'endAt', 'id', 'rules', and 'event' keys.
+        :param league: Dictionary. Should contain 'id', 'description',
+            'startAt', 'endAt', 'registerAt', 'url', 'rules', and 'event' keys.
         :param cursor: sqlite3.Cursor. A cursor connected to the database that
             contains the table to input leagues into.
         :param table: String. The table to insert leagues into.
         :return: Does not return.
-    '''
+    """
     league_tuple = (unicode(league['id']),
                     unicode(league['description']),
                     unicode(league['startAt']),
@@ -25,11 +25,10 @@ def write_league_to_table(league, cursor, table):
     cursor.execute('''  INSERT OR IGNORE INTO
                         {} VALUES (?,?,?,?,?,?,?,?)
                    '''.format(table), league_tuple)
-    pass
 
 
 def single_call_to_table(league_list, cursor, table):
-    ''' Takes the returned list of leagues in a single API call and inputs them
+    """ Takes the returned list of leagues in a single API call and inputs them
         to a given SQLite table
     
         :param league_list: A list of league dicts. Each dict should have the
@@ -39,14 +38,13 @@ def single_call_to_table(league_list, cursor, table):
             contains the table to input leagues into.
         :param table: String. The table to insert leagues into.
         :return: Does not return.
-    '''
+    """
     for league in league_list:
         write_league_to_table(league, cursor, table)
-    pass
 
 
 def leagues_to_table(cursor, conn, table, leagues_per_call, max_offset):
-    ''' Leagues via the Path of Exile API and inputs them
+    """ Leagues via the Path of Exile API and inputs them
         in an SQLite table
 
         :param cursor: sqlite3.Cursor. A cursor connected to the database that
@@ -60,7 +58,7 @@ def leagues_to_table(cursor, conn, table, leagues_per_call, max_offset):
             retrieve leagues from the API with. If None retrieves results
             until there are no leagues returned.
         :return: Does not return.
-    '''
+    """
     offset = 0
     while True:
         print(offset)
@@ -76,11 +74,10 @@ def leagues_to_table(cursor, conn, table, leagues_per_call, max_offset):
             break
         conn.commit()
         time.sleep(5)  # Wait so that the API does not throttle my requests
-    pass
 
 
 def all_leagues_to_table(cursor, conn, table):
-    ''' Collects all of the leagues via the Path of Exile API and inputs them
+    """ Collects all of the leagues via the Path of Exile API and inputs them
         in an SQLite table
     
         :param cursor: sqlite3.Cursor. A cursor connected to the database that
@@ -89,33 +86,32 @@ def all_leagues_to_table(cursor, conn, table):
             contains the table to input leagues into.
         :param table: String. The SQLite table name to store the leagues.
         :return: Does not return.
-    '''
+    """
     leagues_to_table(cursor, conn, table, leagues_per_call=50, max_offset=None)
-    pass
 
 
 def get_leagues_from_table(cursor, table):
-    ''' Gets all the league ids and return in a list '
+    """ Gets all the league ids and return in a list '
 
          :param cursor: sqlite3.Cursor. A cursor connected to the database that
             contains the table to input leagues into.
         :param table: String. The SQLite table that has the leagues.
         :return: List.
-    '''
+    """
     cursor.execute('SELECT id FROM {}'.format(table))
     ids = [x[0] for x in cursor.fetchall()]
     return ids
 
 
 def ladder_to_table(league_id, cursor, ladder_table):
-    ''' Given a league id, adds all the players in the league to a table
+    """ Given a league id, adds all the players in the league to a table
 
         :param league_id: String.
         :param cursor: sqlite3.Cursor. A cursor connected to the database that
             contains the table to input leagues into.
         :param ladder_table: String. The SQLite table name to store the ladder.
         :return: Does not return.
-    '''
+    """
     ladder = poe_ladder.retrieve(league_id)
     for entry in ladder:
         if 'twitch' in entry['account']:
@@ -138,7 +134,6 @@ def ladder_to_table(league_id, cursor, ladder_table):
         cursor.execute(''' INSERT OR IGNORE INTO {}
                            VALUES (?,?,?,?,?,?,?,?, ?)
                        '''.format(ladder_table), ladder_tuple)
-    pass
 
 
 def all_ladders_to_table(cursor, conn, league_ids, ladder_table, start=0):
